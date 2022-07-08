cf1a7f79ea0fc9e17a14cc209298f2e44815a11d
moet-stein <0101.mste@gmail.com>
2022-07-08 15:51:43 +0200

    add SignupPresenterTests file

---
+++ PhotoAppTests/Signup/SignupPresenterTests.swift
@@ -0,0 +1,29 @@
+//
+//  SignupPresenterTests.swift
+//  PhotoAppTests
+//
+//  Created by Moe Steinmueller on 08.07.22.
+//
+
+import XCTest
+
+class SignupPresenterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    
+    func testSignupPresenter_WhenInformationProvided_WillValidateEachProperty() {
+        //Arrange
+        
+        //Act
+        
+        //Assert 
+    }
+
+}
