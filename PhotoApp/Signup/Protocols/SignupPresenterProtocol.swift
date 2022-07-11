d27c84844ecab63b576be8cccb75ee07199068dc
moet-stein <0101.mste@gmail.com>
2022-07-11 17:53:30 +0200

    implement testViewController_WhenSignupButtonTapped_InvokesSignupProcess to test UIButton action invokes code in Presenter

---
+++ PhotoApp/Signup/Protocols/SignupPresenterProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  SignupPresenterProtocol.swift
+//  PhotoApp
+//
+//  Created by Moe Steinmueller on 11.07.22.
+//
+
+import Foundation
+
+protocol SignupPresenterProtocol: AnyObject {
+    init(formModelValidator: SignupModelValidatorProtocol, webservice: SignupWebServiceProtocol, delegate: SignupViewDelegateProtocol)
+    
+    func processUserSignup(formModel: SignupFormModel)
+}
---
+++ PhotoAppTests/Signup/Mocks/MockSignupPresenter.swift
@@ -0,0 +1,23 @@
+//
+//  MockSignupPresenter.swift
+//  PhotoAppTests
+//
+//  Created by Moe Steinmueller on 11.07.22.
+//
+
+import Foundation
+@testable import PhotoApp
+
+class MockSignupPresenter: SignupPresenterProtocol {
+    
+    var processUserSignupCalled: Bool = false
+    
+    required init(formModelValidator: SignupModelValidatorProtocol, webservice: SignupWebServiceProtocol, delegate: SignupViewDelegateProtocol) {
+        // TODO:
+    }
+    
+    func processUserSignup(formModel: SignupFormModel) {
+        processUserSignupCalled = true
+    }
+    
+}
