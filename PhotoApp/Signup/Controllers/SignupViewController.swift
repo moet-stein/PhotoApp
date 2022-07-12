9729beb99ee0e1c212b90b21e66f0b65b11361be
moet-stein <0101.mste@gmail.com>
2022-07-12 14:29:35 +0200

    add usernameTextField in Main and VC

--- PhotoApp/Signup/Controllers/SignupViewController.swift
+++ PhotoApp/Signup/Controllers/SignupViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class SignupViewController: UIViewController {
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var repeatPasswordTextField: UITextField!
