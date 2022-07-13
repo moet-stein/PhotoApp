c6815ac4c44e44bd68a7d4b1356c0a82d8784adc
moet-stein <0101.mste@gmail.com>
2022-07-13 15:49:20 +0200

    implement testViewController_WhenValidFormSubmitted_PresentesSuccessDialog

--- PhotoAppUITests/SignupFlowUITests.swift
+++ PhotoAppUITests/SignupFlowUITests.swift
@@ -87,6 +87,34 @@ class SignupFlowUITests: XCTestCase {
         // Assert
         XCTAssertTrue(app.alerts["errorAlertDialog"].waitForExistence(timeout: 1), "An Error ALert dialog was not presented when invalid signup form was submitted")
     }
+    
+    func testViewController_WhenValidFormSubmitted_PresentesSuccessDialog() {
+        // Arrange
+    
+        firstName.tap()
+        firstName.typeText("Moe")
+        
+        lastName.tap()
+        lastName.typeText("Stein")
+        
+        username.tap()
+        username.typeText("mstein")
+        
+        email.tap()
+        email.typeText("test@test.com")
+        
+        password.tap()
+        password.typeText("12345678")
+        
+        repeatPassword.tap()
+        repeatPassword.typeText("12345678")
+        
+        // Act
+        signupButton.tap()
+        
+        // Assert
+        XCTAssertTrue(app.alerts["successAlertDialog"].waitForExistence(timeout: 1), "An Success Alert dialog was not presented when valid signup form was submitted")
+    }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
